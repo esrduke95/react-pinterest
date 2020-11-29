9cdf05f92a4f6200ad922b5b4219a2ccdeba6431
Summer Duke <esrduke@gmail.com>
2020-11-29 14:57:28 -0600

    forms and views

--- src/helpers/data/pinData.js
+++ src/helpers/data/pinData.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'https://fir-cows-958ae.firebaseio.com/pinterest-webpack';
+const baseUrl = 'https://finterest-3004c.firebaseio.com/';
 
 const getBoardPins = (boardId) => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/pins-boards.json?orderBy="boardId"&equalTo="${boardId}"`).then((response) => {
